df5a9cdc8b22329507a4607db17b2d50a95990ac
Agussstin13 <agusgromo2000@hotmail.com>
2025-04-14 15:56:18 -0300

    progreso para las notificaciones push

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,8 +1,41 @@
+import { useEffect } from 'react'
 import Carousel from '../components/Carousel'
 import MapBranch from '../components/MapBranch'
+import { urlBase64ToUint8Array } from '../utils/vapidConverter';
 
 export default function Menu() {
-  
+  useEffect(() => {
+    try {
+      if (!('Notification' in window) || !('serviceWorker' in navigator) || !('PushManager' in window)) return;
+      (async () => {
+        let registration = await navigator.serviceWorker.register('/service-worker.js');
+
+        let existingSubscription = await registration.pushManager.getSubscription();
+        if (existingSubscription) return;
+
+        let permission = await Notification.requestPermission();
+        if (permission !== 'granted') return;
+
+        let response = await fetch('/push/public-key');
+        let vapidPublicKey = await response.text();
+        let convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+
+        let subscription = await registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidKey,
+        });
+
+        await fetch('/api/subscribe', {
+          method: 'POST',
+          body: JSON.stringify(subscription),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      })()
+    } catch { }
+  }, []);
+
   return (
     <>
       <MapBranch />
