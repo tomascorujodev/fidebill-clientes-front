d2512c14b03ed07ade28d667cb37bcf09d4638ef
TuNombreDeUsuario <agusgromo2000@hotmail.com>
2025-04-14 22:56:06 -0300

    creacion del service worker y peticion de suscripcion a testear

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -2,38 +2,33 @@ import { useEffect } from 'react'
 import Carousel from '../components/Carousel'
 import MapBranch from '../components/MapBranch'
 import { urlBase64ToUint8Array } from '../utils/vapidConverter';
+import { POST } from '../services/Fetch';
 
 export default function Menu() {
   useEffect(() => {
-    try {
-      if (!('Notification' in window) || !('serviceWorker' in navigator) || !('PushManager' in window)) return;
-      (async () => {
-        let registration = await navigator.serviceWorker.register('/service-worker.js');
+    setTimeout(() => {
+      try {
+        if (!('Notification' in window) || !('serviceWorker' in navigator) || !('PushManager' in window)) return;
+        (async () => {
+          let registration = await navigator.serviceWorker.register('/serviceWorker.js');
 
-        let existingSubscription = await registration.pushManager.getSubscription();
-        if (existingSubscription) return;
+          let existingSubscription = await registration.pushManager.getSubscription();
+          if (existingSubscription) return;
 
-        let permission = await Notification.requestPermission();
-        if (permission !== 'granted') return;
+          let permission = await Notification.requestPermission();
+          if (permission !== 'granted') return;
+          let vapidPublicKey = "BPHTg1FW55e8JRPlDXebebv3KOtRaLs5BkmZjTHoz42pHchn7_uG-wv2XJ2wrQKhRrRTTYvse1ZfeGj6I44yO5I"
+          let convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
 
-        let response = await fetch('/push/public-key');
-        let vapidPublicKey = await response.text();
-        let convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+          let subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: convertedVapidKey,
+          });
 
-        let subscription = await registration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: convertedVapidKey,
-        });
-
-        await fetch('/api/subscribe', {
-          method: 'POST',
-          body: JSON.stringify(subscription),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-      })()
-    } catch { }
+          await POST('notificaciones/suscribirse', subscription );
+        })()
+      } catch { }
+    }, 2000)
   }, []);
 
   return (
