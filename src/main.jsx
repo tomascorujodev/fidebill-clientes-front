94c65a948766f4d457035747ea1bad524921ad72
TuNombreDeUsuario <agusgromo2000@hotmail.com>
2025-04-23 06:45:43 -0300

    cargado de las rutas mas dinamico, eficiente y amigable para el usuario. Iconos de Mapbranch de cada empresa. Se arreglo el bug que sacaba el modal de notificaciones si te ibas a otra seccion antes de que salga.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./assets/css/index.css";
 import App from './App.jsx';
-
+import { EmpresaProvider } from "./contexts/EmpresaContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <EmpresaProvider>
+      <App />
+    </EmpresaProvider>
   </StrictMode>
 )
