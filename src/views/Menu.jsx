701453a37460ce2df3175340ce4f53f7aafeb6c5
TuNombreDeUsuario <agusgromo2000@hotmail.com>
2025-04-23 22:59:46 -0300

    boton + en mobilenavbar. Diferenciador de navegador.

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -3,13 +3,32 @@ import MapBranch from '../components/MapBranch'
 
 
 export default function Menu() {
-
+  const userAgent = navigator.userAgent;
+  let navegador;
+  switch (true) {
+    case userAgent.includes("Edg"):
+      navegador = "Edge";
+      break;
+    case userAgent.includes("Chrome") && !userAgent.includes("Edg"):
+      navegador = "Chrome";
+      break;
+    case userAgent.includes("Firefox"):
+      navegador = "Firefox";
+      break;
+    case userAgent.includes("Safari") && !userAgent.includes("Chrome"):
+      navegador = "Safari";
+      break;
+    case userAgent.includes("OPR") || userAgent.includes("Opera"):
+      navegador = "Opera";
+      break;
+    default:
+      navegador = "Otro o no identificado";
+  }
   return (
     <>
       <MapBranch />
       <br />
       <Carousel />
-
     </>
   )
 }
